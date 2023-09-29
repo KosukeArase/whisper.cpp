163f5ff2d8493831b014bb2246ec2c028937bd0b
KosukeArase <kosuke.arase@gmail.com>
2023-09-29 17:21:12 +0200

    small model

--- examples/whisper.objc/whisper.objc/ViewController.m
+++ examples/whisper.objc/whisper.objc/ViewController.m
@@ -59,7 +59,7 @@ void AudioInputCallback(void * inUserData,
     // whisper.cpp initialization
     {
         // load the model
-        NSString *modelPath = [[NSBundle mainBundle] pathForResource:@"ggml-base" ofType:@"bin"];
+        NSString *modelPath = [[NSBundle mainBundle] pathForResource:@"ggml-small" ofType:@"bin"];
 
         // check if the model exists
         if (![[NSFileManager defaultManager] fileExistsAtPath:modelPath]) {
